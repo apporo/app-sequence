c302b1535f0006e2f176c2f4ba8bf6274a5afc64
pnhung177 <pnhung177@acegik.net>
2019-07-26 18:30:44 +0700

    Enhance the generate() function

---
+++ test/app/README.md
@@ -0,0 +1,13 @@
+# app-sequence test/app
+
+> Devebot auto-increment generation layerware
+
+## Usage
+
+### Run the server for testing
+
+```shell
+export LOGOLITE_DEBUGLOG_ENABLED=true
+export DEBUG=devebot*,app*
+npm run build && node test/app
+```
--- test/app/config/sandbox.js
+++ test/app/config/sandbox.js
@@ -3,24 +3,22 @@
 var path = require('path');
 var contextPath = '/sequence';
 var apiPath = 'api';
-var apiVersion = 'v1';
 
 module.exports = {
   plugins: {
     appRestfront: {
       contextPath: contextPath,
       apiPath: apiPath,
-      apiVersion: apiVersion,
       mappingStore: {
         "example-mappings": path.join(__dirname, '../lib/mappings/all.js')
       }
     },
     appTracelog: {
-      tracingPaths: [ path.join(contextPath, apiPath, apiVersion) ],
+      tracingPaths: [ path.join(contextPath, apiPath) ],
       tracingBoundaryEnabled: true
     },
     appSequence: {
-      counterStateKey: 'sequence-counter'
+      counterStateKey: 'dev:sequence-counter'
     },
     appWebserver: {
       port: 17771
